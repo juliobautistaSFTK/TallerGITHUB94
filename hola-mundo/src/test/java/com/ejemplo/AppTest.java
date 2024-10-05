e65a0cd7bfe1703249930f301673b03951dd87ba
juliobautista1 <bacj68@msn.com>
2024-10-05 14:04:38 -0600

    carga codigo

---
+++ hola-mundo/src/test/java/com/ejemplo/AppTest.java
@@ -0,0 +1,11 @@
+package com.ejemplo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("¡Hola Mundo!", "¡Hola Mundo!"); // Ejemplo de prueba
+    }
+}
