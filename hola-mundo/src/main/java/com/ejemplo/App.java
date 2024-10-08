d1f932f51c0322dbb13c430ff4358e91c093336c
juliobautista1 <121524350+juliobautista1@users.noreply.github.com>
2024-10-08 13:29:32 -0600

    Update App.java para participante y para action

--- hola-mundo/src/main/java/com/ejemplo/App.java
+++ hola-mundo/src/main/java/com/ejemplo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! desde alumno" );
+        System.out.println( "Hello World! desde rama participante para action" );
     }
 }
